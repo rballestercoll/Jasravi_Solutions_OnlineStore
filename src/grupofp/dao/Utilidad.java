ce6d0dc5dcb83650379061f7a14026760fde539d
Rafel Ballester Coll <rballestercoll@gmail.com>
2023-11-28 21:47:12 +0100

    Producto finalizado

--- src/grupofp/dao/Utilidad.java
+++ src/grupofp/dao/Utilidad.java
@@ -7,7 +7,7 @@ public class Utilidad {
     protected Connection conexion;
     private final String url = "jdbc:mysql://localhost:3306/onlinestore";
     private final String user = "root";
-    private final String password = "recelus777";
+    private final String password = "1234";
     private final String driver = "com.mysql.cj.jdbc.Driver";
     Connection cx;
     public Utilidad(){
